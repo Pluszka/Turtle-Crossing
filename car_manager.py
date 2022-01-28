f0484e5aad275de6fbfe00879caae6b9f4cb7d9f
Pliszka <karoliniakkjk@gmail.com>
2022-01-28 13:18:54 +0100

    random  car attributes

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
+from random import choice
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+CARS_LEN = [2,3,4]
+
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.create()
 
 
-class CarManager:
-    pass
+    def create(self):
+        self.shape('square')
+        self.color(choice(COLORS))
+        self.shapesize(stretch_wid=1, stretch_len= choice(CARS_LEN))
+
